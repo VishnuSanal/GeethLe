241c0527437835c9c6e1b22310b2faf1a181d214
VishnuSanal <t.v.s10123@gmail.com>
2024-05-12 00:11:33 +0530

    update landing page

---
+++ templates/script.js
@@ -0,0 +1,13 @@
+// window.addEventListener("DOMContentLoaded", function (e) {
+
+//     this.document.getElementById('search_button').addEventListener("click", function () {
+
+//         text = document.getElementById('search_input').value
+
+//         if (text.trim() === "") {
+//             document.getElementById('search_button').innerText = "Query empty!"
+//             return
+//         }
+
+//     })
+// })
