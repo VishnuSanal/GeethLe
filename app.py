6dd10dfa8ac279a8e07839a89947689d0a056e21
VishnuSanal <t.v.s10123@gmail.com>
2024-05-13 15:51:00 +0530

    improve landing screen

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 
 import geeth_le
 
@@ -28,13 +28,24 @@ def generate_from_spotify(spotify_track_id):
 @app.route('/<query>')
 @app.route('/search/<query>')
 def search_music(query):
-    if len(query) == 0:
+    if len(query) == 0 or query == "favicon.ico":
         return render_template('landing.html')
 
     title, description, frame_url, redirect_url = geeth_le.search_music(query)
 
     metadata = {'title': title, 'description': description, 'frame_url': frame_url, "redirect_url": redirect_url}
     return render_template('index.html', metadata=metadata)
+
+
+@app.route('/get_thumb_from_query/<query>')
+def get_thumb_from_query(query):
+    if len(query) == 0:
+        return
+
+    title, description, frame_url, redirect_url = geeth_le.search_music(query)
+
+    metadata = {'title': title, 'description': description, 'frame_url': frame_url, "redirect_url": redirect_url}
+    return jsonify(metadata)
 
 
 @app.route('/')
