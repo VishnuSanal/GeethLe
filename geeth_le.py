16033b70fb86b8164bf2955c81e74eb52c55d178
VishnuSanal <t.v.s10123@gmail.com>
2024-06-17 20:27:36 +0530

    small changes :trademark:!

--- geeth_le.py
+++ geeth_le.py
@@ -80,11 +80,11 @@ def generate(entity_id, album, title, artist, thumbnail_url):
 
     description_wrapped = f'{album_wrapped[0]} • {artist_wrapped[0]}'
 
-    text_draw.text(xy=(250, 415), align='centre', text=title_wrapped[0], fill=(233, 227, 241),
+    text_draw.text(xy=(250, 415), align='center', text=title_wrapped[0], fill=(233, 227, 241),
                    font=ImageFont.truetype('geist_medium.ttf', 32),
                    anchor="mm")
 
-    text_draw.text(xy=(250, 455), align='centre', text=description_wrapped, fill=(233, 227, 241),
+    text_draw.text(xy=(250, 455), align='center', text=description_wrapped, fill=(233, 227, 241),
                    font=ImageFont.truetype('geist_medium.ttf', 18),
                    anchor="mm")
 
